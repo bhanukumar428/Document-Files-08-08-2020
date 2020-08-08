cad8ba3bd1f3cae523a3fc5ea644ce6d1ce0ff6a
bhanukumar428 <69196122+bhanukumar428@users.noreply.github.com>
2020-08-08 01:59:55 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Source Code Management'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'andriod application dev'
+copyright = '2020, masten'
+author = 'bhanukumar'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
